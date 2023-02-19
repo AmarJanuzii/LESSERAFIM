62313ab7ee54ce27a12203920624f9a1a9a6c8a6
AmarJanuzii <78106627+AmarJanuzii@users.noreply.github.com>
2023-02-19 14:28:38 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+module.exports = {
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  }
+}
+
+module.exports = {
+
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
+}
+module.exports = {
+
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ]
+
+}
